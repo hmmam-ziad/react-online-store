644082492776e0d6284e6cf388f162b13a00d802
hmmam-ziad <hmmamxxc@gmail.com>
2025-03-19 23:37:17 +0300

    product state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 import ProductCard from "./Compnents/ProductCard"
 import Modal from "./Compnents/ui/Modal"
 import { formInputsList, productList } from "./Data"
 import Button from "./Compnents/ui/Button";
 import Input from "./Compnents/ui/Input";
+import { Description } from "@headlessui/react";
+import { IProduct } from "./interfaces";
 
 
 
 function App() {
-    let [isOpen, setIsOpen] = useState(false)
+    const[product, setProduct] = useState<IProduct>({
+      title: '',
+      description: '',
+      imageURL: '',
+      price: '',
+      colors: [],
+      category: {
+        name: '',
+        imageURL: '',
+      }
+    });  
+
+
+    const [isOpen, setIsOpen] = useState(false)
   
     function open() {
       setIsOpen(true)
@@ -17,12 +32,22 @@ function App() {
     function close() {
       setIsOpen(false)
     }
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value, name} = event.target;
+
+        setProduct({
+          ...product,
+          [name]: value,
+        });
+    };
+
   const renderProduct = productList.map(product => <ProductCard key={product.id} product={product}/>)
   
   const renderFormInputList = formInputsList.map(input => (
     <div className="flex flex-col">
       <label htmlFor={input.id} className="mb-[1px] text-sm font-medium text-gray-700">{input.label}</label>
-      <Input type="text" id={input.id} name={input.name}/>
+      <Input type="text" id={input.id} name={input.name} value={''} onChange={onChangeHandler}/>
     </div>
   ));
 
@@ -33,7 +58,7 @@ function App() {
         <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-md">
           {renderProduct}
         </div>
-        <div className=" bg-white">
+        <div className="bg-white">
           <Modal isOpen={isOpen} close={close} title="Add New Product">
             <form className="space-y-3">
               {renderFormInputList}
